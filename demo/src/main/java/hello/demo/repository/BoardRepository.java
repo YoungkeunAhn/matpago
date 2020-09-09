83794d9ec7b95bb6b30b087c844084954bb04492
herryboro <herryboro@naver.com>
2020-09-09 12:31:14 +0900

    백엔드 기초 환경 셋팅

---
+++ demo/src/main/java/hello/demo/repository/BoardRepository.java
@@ -0,0 +1,13 @@
+package hello.demo.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+@Repository
+public interface BoardRepository {
+    List<Map<String, Object>> getBoards();
+}
